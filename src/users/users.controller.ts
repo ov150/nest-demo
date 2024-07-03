40d3fbec9e511c63b6fe75eff45e0dbe7ee74dc6
ov150 <ov5010143@gmail.com>
2024-07-03 23:04:30 +0530

    demo application

---
+++ src/users/users.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+    @Get()
+    findAll() {
+        return []
+    }
+    @Get(':id') {
+
+}
+}
